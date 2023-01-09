834b526fc0eaa22594465f74fffbbb32d84e8195
HyungDong <mansa0805@gmail.com>
2023-01-09 19:15:26 +0900

    프로그래머스 카카오 블라인드 2023 1번 문제

---
+++ src/Programmers/KaKaoBlind2023_1.java
@@ -0,0 +1,54 @@
+package Programmers;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class KaKaoBlind2023_1 {
+	public int[] solution(String today, String[] terms, String[] privacies) {
+		ArrayList<Integer> list = new ArrayList<>();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+		LocalDate todayDt = LocalDate.parse(today,formatter);
+
+		HashMap<String,Integer> typeMap = new HashMap<>();
+
+		for (String term : terms) {
+			StringTokenizer st = new StringTokenizer(term);
+			typeMap.put(st.nextToken(),Integer.parseInt(st.nextToken()));
+		}
+
+		for (int i = 0; i < privacies.length; i++) {
+			StringTokenizer st = new StringTokenizer(privacies[i]);
+			LocalDate inputDt = LocalDate.parse(st.nextToken(),formatter);
+			String inputType = st.nextToken();
+
+			LocalDate test1 = inputDt.plusMonths(typeMap.get(inputType).intValue()).minusDays(1);
+			LocalDate test2 = todayDt;
+			if (inputDt.plusMonths(typeMap.get(inputType).intValue()).minusDays(1).isBefore(todayDt)) {
+				list.add(i+1);
+			}
+		}
+
+		int[] answer = new int[list.size()];
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = list.get(i);
+		}
+		return answer;
+	}
+}
+
+class Test2023_1 {
+	public static void main(String[] args) {
+		String today = "2022.05.19";
+		String[] terms = {
+			"A 6", "B 12", "C 3"
+		};
+		String[] privacies = {
+			"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"
+		};
+		KaKaoBlind2023_1 kaKaoBlind2023_1 = new KaKaoBlind2023_1();
+		kaKaoBlind2023_1.solution(today,terms,privacies);
+	}
+}
